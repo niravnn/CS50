55b47338b5ade38a579a41d2c5f69950d7ac5cd4
Nirav Shukla <niravmshukla@gmail.com>
2023-01-07 16:38:29 +0530

    Add files via upload

---
+++ sentimental-cash/cash.py
@@ -0,0 +1,28 @@
+import cs50
+
+while True:
+    dollars = cs50.get_float('Change owed: ')
+    if dollars > 0:
+        cents = round(dollars * 100)
+        coins = 0
+
+        # Get quarters values (25¢)
+        quarters = int(cents / 25)
+        # Calulate dime value (10¢)
+        dime = int((cents % 25) / 10)
+        # Calulate nickels value (5¢)
+        nickels = int(((cents % 25) % 10) / 5)
+        # Calulate pennies value (1¢)
+        pennies = int(((cents % 25) % 10) % 5)
+
+        # Don't overthink it, if the cents is less than 25, the coins of quarters is zero
+        coins = coins + quarters
+        if dime > 0:
+            coins = coins + dime
+        if nickels > 0:
+            coins = coins + nickels
+        if pennies > 0:
+            coins = coins + pennies
+
+        print(coins)
+        break
